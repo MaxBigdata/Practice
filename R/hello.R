60e3657bb252f685760b6d88b7bced04c22dd650
Max <familybjp@163.com>
2018-11-08 19:30:29 +0800

    test to output the input number

---
+++ R/hello.R
@@ -0,0 +1,30 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+hello <- function(name) {
+
+  # consul <- new.env()
+  # consul$host = Sys.getenv("consul.host")
+  # consul$port = Sys.getenv("consul.port")
+  # consul$swagger = Sys.getenv("consul.swagger")
+  #
+  # #connect the library 'laputas'
+  # fcast <- est_mysql_conn("Forecast")
+  #
+  # data1=head(DBI::dbListTables(fcast), 10)
+  print(name)
+
+  # DBI::dbDisconnect(fcast)
+}
